3ce2c05aecd08ec7ed736319d29cdd935819d6f7
Your Name <you@example.com>
2025-03-20 18:30:40 +0700

    init

---
+++ src/testProject.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stats.h"
+ int main(){
+ 	void calculate_revenue_by_day();
+	void calculate_revenue_by_month();
+
+	void find_food_best_selling();
+	void find_drink_best_selling();
+ 	return 0;
+ }
+ 
