336f56885c1e6b72b797e6f92b12a7e047b454c3
Your Name <you@example.com>
2025-04-01 15:11:04 +0700

    add code test

---
+++ test/testmain.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stats.h"
+#include <gtk/gtk.h>
+
+void apply_css(GtkWidget *window) {
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(provider, "Glade_CSS/stats.css", NULL);
+    
+    GtkStyleContext *context = gtk_widget_get_style_context(window);
+    gtk_style_context_add_provider_for_screen(
+        gdk_screen_get_default(),
+        GTK_STYLE_PROVIDER(provider),
+        GTK_STYLE_PROVIDER_PRIORITY_USER);
+}
+
+// Các biến toàn cục để truy cập widget từ nhiều hàm
+GtkLabel *label_daily, *label_monthly, *label_best_food, *label_best_drink;
+
+// Hàm cập nhật số liệu lên giao diện
+void update_stats(GtkWidget *widget, gpointer data) {
+    char buffer[256];
+
+    int daily_revenue = calculate_revenue_by_day();
+    int monthly_revenue = calculate_revenue_by_month();
+    char *best_food = find_food_best_selling();
+    char *best_drink = find_drink_best_selling();
+
+    if (daily_revenue >= 0) {
+        sprintf(buffer, "Daily Revenue: %d VND", daily_revenue);
+        gtk_label_set_text(label_daily, buffer);
+    }
+
+    if (monthly_revenue >= 0) {
+        sprintf(buffer, "Monthly Revenue: %d VND", monthly_revenue);
+        gtk_label_set_text(label_monthly, buffer);
+    }
+
+    if (best_food) {
+        sprintf(buffer, "Best Selling Food: %s", best_food);
+        gtk_label_set_text(label_best_food, buffer);
+    }
+
+    if (best_drink) {
+        sprintf(buffer, "Best Selling Drink: %s", best_drink);
+        gtk_label_set_text(label_best_drink, buffer);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    GtkBuilder *builder;
+    GtkWidget *window, *btn_stats;
+
+    gtk_init(&argc, &argv);
+
+    // Load file Glade
+    builder = gtk_builder_new_from_file("UI Glade/UI Stats.glade");
+
+    // Lấy widget từ file Glade
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "Revenue_window"));
+    btn_stats = GTK_WIDGET(gtk_builder_get_object(builder, "btn_stats"));
+
+    label_daily = GTK_LABEL(gtk_builder_get_object(builder, "label_daily_data"));
+    label_monthly = GTK_LABEL(gtk_builder_get_object(builder, "label_monthly_data"));
+    label_best_food = GTK_LABEL(gtk_builder_get_object(builder, "label_best_food"));
+    label_best_drink = GTK_LABEL(gtk_builder_get_object(builder, "label_best_drink"));
+
+    // Gán sự kiện cho nút "Stats"
+    g_signal_connect(btn_stats, "clicked", G_CALLBACK(update_stats), NULL);
+
+    gtk_widget_show_all(window);
+    apply_css(window);
+    gtk_main();
+
+    return 0;
+}
