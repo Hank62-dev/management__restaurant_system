a40966af5e3ae7ce73566ab21957e81a15a8bf77
VoGiaHuy <huyvog6226@gmail.com>
2025-04-19 03:23:24 +0700

    update code test orders

--- test/test.c
+++ test/test.c
@@ -1,120 +1,72 @@
-/*#include <gtk/gtk.h>
-#include <string.h>
-
-GtkBuilder *builder;
-GtkWidget *Orders_Window;
-GtkListStore *list_store;
-GtkTreeView *orders_view;
-
-// Load CSS
-void load_css_orders() {
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "Glade_CSS/orders.css", NULL); // Đường dẫn tới file CSS
-
-    GdkDisplay *display = gdk_display_get_default();
-    GdkScreen *screen = gdk_display_get_default_screen(display);
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
-}
-
-// Load dữ liệu từ view_bill.txt vào TreeView
-void load_orders_data(const char *filename) {
-    GtkListStore *list_store = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING); // 4 cột
-    GtkTreeIter iter;
-
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        g_warning("Không thể mở file %s: %s", filename, strerror(errno));
-        return;
-    }
-
-    char name[100], price[100], date[100], id[100];
-    while (fscanf(file, "%s %s %s %s", name, price, date, id) == 4) {
-        printf("Name: %s, Price: %s, Date: %s, ID: %s\n", name, price, date, id);
-        gtk_list_store_append(list_store, &iter);
-        gtk_list_store_set(list_store, &iter,
-            0, name,
-            1, price,
-            2, date,
-            3, id,
-            -1);
-    }
-
-    fclose(file);
-
-    gtk_tree_view_set_model(GTK_TREE_VIEW(orders_view), GTK_TREE_MODEL(list_store));
-    g_object_unref(list_store);
-}
-
-// Khởi tạo giao diện Orders
-void setup_orders_ui() {
-    builder = gtk_builder_new_from_file("UI Glade/UI Orders.glade");
-
-    Orders_Window = GTK_WIDGET(gtk_builder_get_object(builder, "Orders_Window"));
-    orders_view = GTK_TREE_VIEW(gtk_builder_get_object(builder, "orders_view"));
-
-    load_css_orders();
-    load_orders_data("data/view_bill.txt");
-
-    gtk_widget_show_all(Orders_Window);
-}
-*/
-
 #include <gtk/gtk.h>
+#include <stdio.h>
 #include <string.h>
-#include <errno.h> 
 
-GtkBuilder *builder;
-GtkWidget *Orders_Window;
-GtkTreeView *orders_view;
+#define MAX_ROWS 100  // Số dòng tối đa từ file
 
-// Load CSS
-void load_css_orders() {
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "Glade_CSS/orders.css", NULL);
-
-    GdkDisplay *display = gdk_display_get_default();
-    GdkScreen *screen = gdk_display_get_default_screen(display);
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+void apply_css(GtkWidget *widget, GtkCssProvider *provider) {
+    GtkStyleContext *context = gtk_widget_get_style_context(widget);
+    gtk_style_context_add_provider(context,
+        GTK_STYLE_PROVIDER(provider),
+        GTK_STYLE_PROVIDER_PRIORITY_USER);
 }
 
-// Load dữ liệu từ view_bill.txt vào TreeView
-void load_orders_data(const char *filename) {
-    GtkListStore *store = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING); // 4 cột
-    GtkTreeIter iter;
+void load_css(const char *css_path, GtkWidget *widget) {
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(provider, css_path, NULL);
+    apply_css(widget, provider);
+    g_object_unref(provider);
+}
 
-    FILE *file = fopen(filename, "r");
+void load_labels_from_file(GtkBuilder *builder) {
+    FILE *file = fopen("data/view_bill.txt", "r");
     if (!file) {
-        g_warning("Không thể mở file %s: %s", filename, strerror(errno));
+        g_print("Không thể mở file view_bill.txt\n");
         return;
     }
 
-    char name[100], price[100], date[100], id[100];
-    while (fscanf(file, "%s %s %s %s", name, price, date, id) == 4) {
-        printf("Name: %s, Price: %s, Date: %s, ID: %s\n", name, price, date, id);
-        gtk_list_store_append(store, &iter);
-        gtk_list_store_set(store, &iter,
-            0, name,
-            1, price,
-            2, date,
-            3, id,
-            -1);
+    char name[100], price[50], date[50], id[50];
+    int i = 1;
+
+    while (fscanf(file, "%s %s %s %s", name, price, date, id) == 4 && i <= MAX_ROWS) {
+        char id_label[32], name_label[32], date_label[32], price_label[32];
+
+        sprintf(id_label, "ID%d", i);
+        sprintf(name_label, "name%d", i);
+        sprintf(date_label, "date%d", i);
+        sprintf(price_label, "price%d", i);
+
+        GtkLabel *label_id = GTK_LABEL(gtk_builder_get_object(builder, id_label));
+        GtkLabel *label_name = GTK_LABEL(gtk_builder_get_object(builder, name_label));
+        GtkLabel *label_date = GTK_LABEL(gtk_builder_get_object(builder, date_label));
+        GtkLabel *label_price = GTK_LABEL(gtk_builder_get_object(builder, price_label));
+
+        if (label_id && label_name && label_date && label_price) {
+            gtk_label_set_text(label_id, id);
+            gtk_label_set_text(label_name, name);
+            gtk_label_set_text(label_date, date);
+            gtk_label_set_text(label_price, price);
+        } else {
+            g_print("Không tìm thấy label cho dòng %d\n", i);
+        }
+
+        i++;
     }
 
     fclose(file);
-
-    gtk_tree_view_set_model(GTK_TREE_VIEW(orders_view), GTK_TREE_MODEL(store));
-    g_object_unref(store); 
 }
 
-// Khởi tạo giao diện Orders
-void setup_orders_ui() {
-    builder = gtk_builder_new_from_file("UI Glade/UI Orders.glade");
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
 
-    Orders_Window = GTK_WIDGET(gtk_builder_get_object(builder, "Orders_Window"));
-    orders_view = GTK_TREE_VIEW(gtk_builder_get_object(builder, "orders_view"));
-    
-    load_css_orders();
-    load_orders_data("data/view_bill.txt");
+    GtkBuilder *builder = gtk_builder_new_from_file("UI_Glade/UI Orders.glade");
+    GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "orders_window"));
 
-    gtk_widget_show_all(Orders_Window);
+    load_css("Glade_CSS/orders.css", window);
+    load_labels_from_file(builder);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
 }
