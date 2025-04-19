c4bf85ae8379aabe446869625118fd6bcb4b696c
VoGiaHuy <huyvog6226@gmail.com>
2025-04-19 13:25:58 +0700

    update code huy

--- src/total_bills.c
+++ src/total_bills.c
@@ -1,4 +1,3 @@
-
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,6 +6,9 @@
 #define MAX_ROWS 100
 #define MAX_LINE_LEN 256
 
+GtkWidget *btn_orders;
+
+// Hàm apply CSS
 void load_css_orders(const char *css_path) {
     GtkCssProvider *provider = gtk_css_provider_new();
     gtk_css_provider_load_from_path(provider, css_path, NULL);
@@ -19,10 +21,10 @@ void load_css_orders(const char *css_path) {
     g_object_unref(provider);
 }
 
+// hàm lấy dữ liệu từ file
+void load_labels_from_file(GtkButton *button, gpointer user_data) {
+    GtkBuilder *builder = GTK_BUILDER(user_data);
 
-
-
-void load_labels_from_file(GtkBuilder *builder) {
     FILE *file = fopen("data/view_bill.txt", "r");
     if (!file) {
         g_print("Not open file view_bill.txt\n");
@@ -35,36 +37,31 @@ void load_labels_from_file(GtkBuilder *builder) {
     while (fgets(line, sizeof(line), file) && i <= MAX_ROWS) {
         char id[50], name[100], date[50], price[50];
 
-        // Gỡ xuống dòng \n nếu có
         line[strcspn(line, "\n")] = 0;
 
-        // Phân tích dòng bằng sscanf
         if (sscanf(line, "%s %s %s %s", id, name, date, price) != 4) {
             g_print("Dòng %d không hợp lệ: %s\n", i, line);
             continue;
         }
 
-        // Tạo ID label name như trong Glade
         char id_label[32], name_label[32], date_label[32], price_label[32];
         sprintf(id_label, "id%d", i);
         sprintf(name_label, "name%d", i);
         sprintf(date_label, "date%d", i);
         sprintf(price_label, "price%d", i);
 
-        // Lấy widget từ Glade
         GtkLabel *label_id = GTK_LABEL(gtk_builder_get_object(builder, id_label));
         GtkLabel *label_name = GTK_LABEL(gtk_builder_get_object(builder, name_label));
         GtkLabel *label_date = GTK_LABEL(gtk_builder_get_object(builder, date_label));
         GtkLabel *label_price = GTK_LABEL(gtk_builder_get_object(builder, price_label));
 
-        // Nếu đủ label thì in ra
         if (label_id && label_name && label_date && label_price) {
             gtk_label_set_text(label_id, id);
             gtk_label_set_text(label_name, name);
             gtk_label_set_text(label_date, date);
             gtk_label_set_text(label_price, price);
         } else {
-            g_print("Not found label for %d\n", i);
+            g_print("Không tìm thấy label cho dòng %d\n", i);
         }
 
         i++;
@@ -73,16 +70,17 @@ void load_labels_from_file(GtkBuilder *builder) {
     fclose(file);
 }
 
+// Hàm chính hiện toàn bộ bills
 void total_bills() {
-
     GtkBuilder *builder = gtk_builder_new_from_file("UI Glade/UI Orders.glade");
     GtkWidget *window_bills = GTK_WIDGET(gtk_builder_get_object(builder, "Orders_Window"));
 
+    btn_orders = GTK_WIDGET(gtk_builder_get_object(builder, "btn_orders"));
+
+   //kết nối sự kiện
+    g_signal_connect(btn_orders, "clicked", G_CALLBACK(load_labels_from_file), builder);
+
     load_css_orders("Glade_CSS/orders.css");
-    load_labels_from_file(builder);
 
     gtk_widget_show_all(window_bills);
- 
 }
-
-
